81ce2f1d10f8b470f5b359a5af550d42b1d1acba
Natalie Lockyer <Natalie_lockyer@outlook.com>
2024-01-29 16:41:46 +0000

    Added email

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -63,7 +63,11 @@ function fetchGitHubInformation(event) {
         },
         function (errorResponse) {
             if (errorResponse.status === 404) {
-                $("gh-user-data").html(`<h2>No info forun for user ${username}</h2>`);
+                $("gh-user-data").html(
+                    `<h2>No info found for user ${username}</h2>`);
+            } else if (errorResponse.status === 403) {
+                var resetTime = new Date(errorResponse.getResponseHeader('X-RateLimit-Reset') * 1000);
+                $("#gh-user-data").html(`<h4>Too many requests, please wait until ${resetTime.toLocaleTimeString()}</h4>`);
             } else {
                 console.log(errorResponse);
                 $("#gh-user-data").html(
